6ddf793416bb0fd48961da6f61611fe1e012719a
Lautaro Amengual <amengual.ln@gmail.com>
2024-12-19 10:28:33 -0300

    better new item modal :)

--- src/components/NewItemModal.jsx
+++ src/components/NewItemModal.jsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import CameraIcon from "./icons/camera";
 
 export default function NewItemModal({ onClose, onSubmit }) {
   const [imagePreview, setImagePreview] = useState("");
@@ -11,6 +12,7 @@ export default function NewItemModal({ onClose, onSubmit }) {
   });
 
   const handleImageLoad = (e) => {
+    if (e.target.files.length === 0) return;
     const file = e.target.files[0];
     const imagePreviewURL = URL.createObjectURL(file);
     setImagePreview(imagePreviewURL);
@@ -29,21 +31,34 @@ export default function NewItemModal({ onClose, onSubmit }) {
         <div className="w-[100vw] flex flex-col gap-4 bg-white rounded-lg p-4 border border-gray-200">
           <h1 className="text-xl mb-4">Nueva prenda</h1>
           {imagePreview && (
-            <Image 
-              src={imagePreview} 
-              width={100} 
-              height={100} 
-              alt="Preview" 
-              className="w-full h-full object-cover aspect-square" 
-            />
+            <label for="dropzone-file">
+              <Image 
+                src={imagePreview} 
+                width={100} 
+                height={100} 
+                alt="Preview" 
+                className="w-full h-full object-cover aspect-square" 
+              />
+            </label>
           )}
+          { !imagePreview &&
+          <div class="flex w-full border justify-center items-center">
+            <label for="dropzone-file" class="flex flex-col items-center justify-center w-full p-12 rounded-2xl cursor-pointer">
+              <div class="mb-3 flex items-center justify-center">
+                <CameraIcon width={80} height={80} className="text-gray-400" />
+              </div>
+            </label>
+          </div>}
+
           <input
+            id="dropzone-file"
             type="file"
             accept="image/*"
             capture="camera"
             onChange={handleImageLoad}
-            className="w-full border p-4 text-xl outline-none"
+            className="hidden"
           />
+
           <input
             value={item.name}
             onChange={(e) => setItem({ ...item, name: e.target.value })}
